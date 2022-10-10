6d9eac800902fe3459afcdc95a5eb8a2cad6769e
brunstighanhval <mlkaer1@hotmail.com>
2022-10-10 12:05:22 +0200

    HighScoreController/View
    HighScoreController/View

---
+++ TicTacToe/src/tictactoe/gui/controller/HighScoreController.java
@@ -0,0 +1,17 @@
+package tictactoe.gui.controller;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+public class HighScoreController extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public void Hey(ActionEvent actionEvent) {
+    }
+}
