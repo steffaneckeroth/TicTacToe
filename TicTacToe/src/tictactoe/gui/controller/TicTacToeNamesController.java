b1289bd2d387887febf836960ba2806c9db3d01b
KasperTell <kaspertell@gmail.com>
2022-10-11 09:33:17 +0200

    Name input

---
+++ TicTacToe/src/tictactoe/gui/controller/TicTacToeNamesController.java
@@ -0,0 +1,10 @@
+package tictactoe.gui.controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class TicTacToeNamesController {
+    public TextField Player1;
+    public TextField Player2;
+    public Button btnSubmit;
+}
---
+++ TicTacToe/src/tictactoe/gui/controller/TicTacToeSingleplayerNameController.java
@@ -0,0 +1,9 @@
+package tictactoe.gui.controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class TicTacToeSingleplayerNameController {
+    public TextField SinglePlayerName;
+    public Button btnSinglePlayerSubmit;
+}
