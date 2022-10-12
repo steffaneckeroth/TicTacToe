39b4d5abb34cbe520f5028a9b4d7156e5f89634a
brunstighanhval <mlkaer1@hotmail.com>
2022-10-12 09:20:56 +0200

    HighScore BTN

--- TicTacToe/src/tictactoe/gui/controller/HighscoreController.java
+++ TicTacToe/src/tictactoe/gui/controller/HighscoreController.java
@@ -2,19 +2,39 @@ package tictactoe.gui.controller;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HighscoreController extends Application {
 
+    public Button btnMenu;
+    public Button btnRetry;
+
+    public Button btnMultiplayer;
 
     @Override
     public void start(Stage primaryStage) {
 
     }
 
-    public void Menu(ActionEvent actionEvent) {
+    public void Menu(ActionEvent actionEvent)throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/tictactoe/gui/views/IntroView.fxml"));
+        Parent root = loader.load();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
-    public void Retry(ActionEvent actionEvent) {
+    public void Retry(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/tictactoe/gui/views/MultiplayerNamesView.fxml"));
+        Parent root = loader.load();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
 }
